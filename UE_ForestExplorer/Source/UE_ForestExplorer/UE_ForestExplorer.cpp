0527c1c8f2f86ee39c50af6304f800b96707386a
Freek Pluim <freekpluim@gmail.com>
2025-09-24 13:01:36 +0200

    Initial Commit

---
+++ UE_ForestExplorer/Source/UE_ForestExplorer/UE_ForestExplorer.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE_ForestExplorer.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE_ForestExplorer, "UE_ForestExplorer" );
+
+DEFINE_LOG_CATEGORY(LogUE_ForestExplorer)
+ 
---
+++ UE_ForestExplorer/Source/UE_ForestExplorer/UE_ForestExplorerGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE_ForestExplorerGameMode.h"
+
+AUE_ForestExplorerGameMode::AUE_ForestExplorerGameMode()
+{
+	// stub
+}
